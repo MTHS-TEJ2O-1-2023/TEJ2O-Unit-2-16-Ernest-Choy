f23c78d3b3e5a66c7f16610b713181af0e76f39e
Ernest Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-12-06 12:42:12 -0500

    tyver

--- main.ts
+++ main.ts
@@ -5,4 +5,19 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+
+// variables
+let distanceToTarget: number = 0
+
+// set up
+radio.setGroup(69)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+while (true) {
+  distanceToTarget = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.Centimeters)
+  if (distanceToTarget < 10) {
+  radio.sendString("Too close") 
+  }
+}
+
